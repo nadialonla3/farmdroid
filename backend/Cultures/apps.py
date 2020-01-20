19d5b94bc21f7201ea0e40d491e52a4d645f3443
unknown <mekogeek@gmail.com>
2020-01-20 09:27:53 +0100

    modif

--- backend/Cultures/apps.py
+++ backend/Cultures/apps.py
@@ -19,11 +19,11 @@ class CulturesConfig(AppConfig):
         type2.save()
 
         parameter1 = CultureParameters(
-            name='temperature', minValue=5, maxValue=50, valueType=type1)
+            name='temperature1', minValue=5, maxValue=50, valueType=type1)
         parameter2 = CultureParameters(
             name='pH', minValue=2, maxValue=20, valueType=type2)
         parameter3 = CultureParameters(
-            name='temperature', minValue=10, maxValue=40, valueType=type1)
+            name='temperature2', minValue=10, maxValue=40, valueType=type1)
         parameter1.save()
         parameter2.save()
         parameter3.save()
@@ -37,6 +37,37 @@ class CulturesConfig(AppConfig):
             position='aposition', topic='sensor/temp', noeudMaitre=master)
         collector1.save()
         collector2.save()
+
+        data1 = Culture(
+            value=100, date=(2019,10,15,15,1,14), parameter= parameter1, 
+            culture=culture1, noeudCollecteur=collector1)
+        data2 = Culture(
+            value=150, date=(2019,10,15,15,1,14), parameter= parameter1, 
+            culture=culture1, noeudCollecteur=collector1)
+        data3 = Culture(
+            value=200, date=(2019,10,18,15,1,14), parameter= parameter1, 
+            culture=culture1, noeudCollecteur=collector1)
+        data4 = Culture(
+            value=250, date=(2019,10,18,15,1,14), parameter= parameter1, 
+            culture=culture1, noeudCollecteur=collector1)
+        data5 = Culture(
+            value=180, date=(2019,10,16,15,1,14), parameter= parameter1, 
+            culture=culture1, noeudCollecteur=collector1)
+        data6 = Culture(
+            value=1, date=(2019,10,15,15,1,14), parameter= parameter2, 
+            culture=culture1, noeudCollecteur=collector1)
+        data7 = Culture(
+            value=5, date=(2019,10,15,15,1,14), parameter= parameter2, 
+            culture=culture1, noeudCollecteur=collector1)
+        data8 = Culture(
+            value=9, date=(2019,10,16,15,1,14), parameter= parameter2, 
+            culture=culture1, noeudCollecteur=collector1)
+        data9 = Culture(
+            value=3, date=(2019,10,15,15,1,14), parameter= parameter2, 
+            culture=culture1, noeudCollecteur=collector1)
+        data10 = Culture(
+            value=14, date=(2019,10,18,15,1,14), parameter= parameter2, 
+            culture=culture1, noeudCollecteur=collector1)
 
         print('Parameters saved')
         return super().ready()
